22a6224a735b70d22cfbf6957741e90d7cc42ddd
SmoollBrain <jakubskowron676@gmail.com>
2021-06-09 23:00:15 +0200

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Release Notes
+
+These are the release notes for my website! ([smoollbrain.github.io](smoollbrain.github.io))
+
+## 09.06.2021 (Development)
+- Added "favicon"
+
+## 09.06.2021
+- Initial start of the website
---
+++ push_changes.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Adding changes
+git add --all
+timeout 3 /nobreak
+cls
+
+echo Commiting changes
+git commit -m "Initial Commit"
+timeout 3 /nobreak
+cls
+
+echo Pushing changes
+git push -u origin main
+timeout /nobreak
